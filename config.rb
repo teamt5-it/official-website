33515cb5962e7e1e0365ad6fb923474cc7650aed
Frank Lai <franklai@teamt5.org>
2019-03-12 18:31:56 +0800

    first version

--- config.rb
+++ config.rb
@@ -37,8 +37,10 @@ page '/*.txt', layout: false
 # Build-specific configuration
 configure :build do
   # Minify CSS on build
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
+
+  activate :asset_hash
 end
